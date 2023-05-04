aa7e57ba44fab953a0cedf8b1f117f50501e7a95
Piyush Garg <piyushgarg.dev@gmail.com>
2023-05-04 19:02:41 +0530

    Changes

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
       "avatars.githubusercontent.com",
       "lh3.googleusercontent.com",
       "piyush-twitter-dev.s3.ap-south-1.amazonaws.com",
+      "i.imgur.com",
     ],
   },
 };
